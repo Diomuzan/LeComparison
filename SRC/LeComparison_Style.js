4695fbf8f73f1135dbda227be32f61bd760c49ce
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 23:05:34 +0200

    Initial commit

---
+++ SRC/LeComparison_Style.js
@@ -0,0 +1,41 @@
+a = 1 
+b = 4
+c = (1 < 4);
+alert(c);
+d = 6
+e = (b > d);
+alert(e)
+f = 'A'
+g = 'g'
+h = (f < g);
+alert(h);
+i = 5
+j = '5'
+k = (i == j);
+alert(k);
+l = (i != j);
+alert(l);
+m = 421
+n = 422
+o = (m >= n);
+alert(o);
+p = 8
+r = '8'
+q = (p === r);
+alert(q)
+s = (p !== r);
+alert(s);
+const x = 5
+const y = 8
+t = (x < y);
+alert(t);
+u = (x == i);
+alert(u);
+v = 'Z'
+w = (y === v);
+alert(w);
+x = 'z'
+y = (y !== x);
+alert(y);
+
+
